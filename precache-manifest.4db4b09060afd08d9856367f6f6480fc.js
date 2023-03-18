8ac9db1076c9ece7ff5fff746552f5367eb736ee
DangoD205 <74743956+DangoD205@users.noreply.github.com>
2023-03-18 03:13:11 -0600

    Updates

---
+++ precache-manifest.4db4b09060afd08d9856367f6f6480fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30794141785548e9b7baddaae62e3d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e037ffb874c34b46bb62",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "4c25e25219a9b7e87372",
+    "url": "/static/css/main.be9b2dec.chunk.css"
+  },
+  {
+    "revision": "e037ffb874c34b46bb62",
+    "url": "/static/js/2.3722babe.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.3722babe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c25e25219a9b7e87372",
+    "url": "/static/js/main.ffe4711c.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
