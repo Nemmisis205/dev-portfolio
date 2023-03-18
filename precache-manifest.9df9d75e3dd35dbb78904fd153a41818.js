0e190284f2fd8658530afe2ec2c0f3acc3ce5f8d
DangoD205 <74743956+DangoD205@users.noreply.github.com>
2023-03-18 02:54:32 -0600

    Updates

--- precache-manifest.9df9d75e3dd35dbb78904fd153a41818.js
+++ precache-manifest.9df9d75e3dd35dbb78904fd153a41818.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce99fce831f9cc238edf170c07efd945",
+    "revision": "1fbf1b896414424f9662639f041776d1",
     "url": "./index.html"
   },
   {
-    "revision": "4ce9afcc4c0d26a531a3",
+    "revision": "e037ffb874c34b46bb62",
     "url": "./static/css/2.d64471e2.chunk.css"
   },
   {
-    "revision": "3b77eaa7848dfd52a6fc",
+    "revision": "61152fc7a96f8dbf267e",
     "url": "./static/css/main.be9b2dec.chunk.css"
   },
   {
-    "revision": "4ce9afcc4c0d26a531a3",
-    "url": "./static/js/2.23a27d4a.chunk.js"
+    "revision": "e037ffb874c34b46bb62",
+    "url": "./static/js/2.3722babe.chunk.js"
   },
   {
     "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
-    "url": "./static/js/2.23a27d4a.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.3722babe.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3b77eaa7848dfd52a6fc",
-    "url": "./static/js/main.4cb12084.chunk.js"
+    "revision": "61152fc7a96f8dbf267e",
+    "url": "./static/js/main.cd8801e6.chunk.js"
   },
   {
     "revision": "1d6896b13d1316b5acb7",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.771c8479119e39a8e9ca0c9c37f4a75c.js"
+  "./precache-manifest.9df9d75e3dd35dbb78904fd153a41818.js"
 );
 
 self.addEventListener('message', (event) => {
